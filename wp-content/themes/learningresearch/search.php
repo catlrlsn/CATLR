fcc06b6a2c4cdc23e69ec4343e94ce22068806eb
cristiandrg96 <cristiandrg96@gmail.com>
2016-01-12 16:51:06 -0500

    Added Alphabetical Sorting to Search

--- wp-content/themes/learningresearch/search.php
+++ wp-content/themes/learningresearch/search.php
@@ -3,15 +3,22 @@
         
         <div id="main" class="col col-lg-4 clearfix" role="main">
             
-            <?php echo '<h4>Order results by date 
+            <?php echo '<h5>Order results by date 
             <a  style=" text-decoration: none;" href="' . get_bloginfo('url') . '?s=' . get_search_query() . '&orderby=post_date&order=asc">
              <i class="fa fa-caret-up" style="margin-left:10px;"></i>
             </a>';
             echo '
             <a  href="' . get_bloginfo('url') . '?s=' . get_search_query() . '&orderby=post_date&order=desc">
              <i class="fa fa-caret-down" style="margin-left:10px;"></i>
-            </a> </h4>';?>
-
+            </a> </h5>';?>
+            <?php echo '<h5>Order results alphabetically 
+            <a  style=" text-decoration: none;" href="' . get_bloginfo('url') . '?s=' . get_search_query() . '&orderby=title&order=asc">
+             <i class="fa fa-caret-up" style="margin-left:5px;"></i>
+            </a>';
+            echo '
+            <a  href="' . get_bloginfo('url') . '?s=' . get_search_query() . '&orderby=title&order=desc">
+             <i class="fa fa-caret-down" style="margin-left:5px;"></i>
+            </a> </h5>';?>
         </div>
         
         
